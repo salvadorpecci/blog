22651559b902297b401b9b78aa3aa7b38dacdd18
salvadorpecci <57363241+salvadorpecci@users.noreply.github.com>
2023-02-08 09:42:44 +0100

    Add files via upload

---
+++ blog/blog_project/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        'Título',
+        max_length=200
+    )
+    author = models.ForeignKey(
+        'auth.User',
+        verbose_name='Autor',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField(
+        'Cuerpo'
+    )
+
+    def __str__(self):
+        return self.title
